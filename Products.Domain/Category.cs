205863076b3ae34f85e387f3d5156b7f8bbf221d
daniel mandarano <danielmandarano@yahoo.com.ar>
2019-03-01 22:11:01 -0300

    primer commit

---
+++ Products.Domain/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Domain
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage ="El campo {0}es requerido")]
+        [MaxLength(50,ErrorMessage ="El campo{0} solo puede tener {1}caracteres de largo")]
+        public string Description { get; set; }
+    }
+}
